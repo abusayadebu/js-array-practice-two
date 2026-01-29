bc3ee910dee7002e8408559299eec5a457c78185
abusayadebu <abusayad743@gmail.com>
2026-01-29 18:08:10 +0600

    looping practice doing again

---
+++ looping.js
@@ -0,0 +1,9 @@
+let friends = ["karim", "rahim", "hasan", "ali"]
+for(let friend of friends){
+    console.log(friend)
+}
+
+for(let i = 0; i < friends.length; i++){
+        // console.log(i)
+        console.log(friends[i])
+}
