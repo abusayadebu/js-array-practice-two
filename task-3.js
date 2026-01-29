f713adc889d75604b0347a85014e823fd3049ce2
abusayadebu <abusayad743@gmail.com>
2026-01-29 19:19:07 +0600

    task-3 array element concatinated in a string

---
+++ task-3.js
@@ -0,0 +1,19 @@
+/**
+ * Task 3
+Use a for...of loop to concatenate all the elements of an array into a single string.
+
+Input: var numbers = ['Tom', 'Tim', 'Tin', 'Tik']
+
+Output:
+
+'TomTimTinTik'
+*/
+
+
+var numbers = ['Tom', 'Tim', 'Tin', 'Tik'];
+let addNumber = " ";
+
+for(let number of numbers){
+    addNumber += number;
+}
+console.log(addNumber)
